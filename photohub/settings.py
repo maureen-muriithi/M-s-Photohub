082951130d64256053a83663dafe77b9583406bf
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-29 01:24:05 +0300

    Create database

--- photohub/settings.py
+++ photohub/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'photohub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photohub',
+        'USER': 'moh',
+        'PASSWORD': 'mjwm2222',
     }
 }
 
