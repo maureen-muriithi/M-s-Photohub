69ac530c11e226013bf401b992a7a5d0d1517d7d
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-29 05:16:09 +0300

    Add tests for the Image model/Complete on tests

--- photos/tests.py
+++ photos/tests.py
@@ -70,6 +70,56 @@ class Test_Category(TestCase):
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
 
+class Test_Image(TestCase):
+    '''
+    Test class for the image model
+    '''
+
+    def setUp(self):
+        self.location = Location(name='Paris')
+        self.location.save_location()
+
+        self.category = Category(name='Travel')
+        self.category.save_category()
+
+        self.image = Image(id=1, name='travelImage', description='This is the eiffel Tower in Paris', location=self.location, category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_image(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image.save_image()
+        self.image.update_photo(self.image.id, 'photos/test.jpg')
+        updated_img = Image.objects.filter(image='photos/test.jpg')
+        self.assertTrue(len(updated_img) > 0)
+
+    def test_filter_image_by_location(self):
+        self.image.save_image()
+        filtered_images = self.image.filter_by_location(location='nairobi')
+        self.assertTrue(len(filtered_images) == 1)
+
+    def test_search_image_by_category(self):
+        category = 'food'
+        searched_img = self.image.search_by_category(category)
+        self.assertTrue(len(searched_img) > 1)
+
+    def tearDown(self):
+        Photographer.all().delete()
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+
     
 
 
