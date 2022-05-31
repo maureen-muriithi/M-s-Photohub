dcf3bb33e1de3d4971737c26e74b8e1225c1dec4
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-31 16:52:38 +0300

    Add database configuration

--- photohub/settings.py
+++ photohub/settings.py
@@ -93,6 +93,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST' : config('DB_HOST'),
+           'PORT' : '',
 
        }
        
