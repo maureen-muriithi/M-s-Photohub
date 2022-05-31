6913fed37df4db418b25a4072c28bf6e59dc2148
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-31 18:00:50 +0300

    Deploy to Heroku

--- photohub/settings.py
+++ photohub/settings.py
@@ -29,7 +29,7 @@ MODE = config('MODE', default='dev')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
